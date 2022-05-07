2159a945bd5c61d151984a90a854f78cfa493de9
Jason Arritt <jsn.arritt@gmail.com>
2022-05-07 15:13:42 -0400

    Got server running

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uuid = require('uuid');
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
