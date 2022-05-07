47c8f4541b7018153741ce2e300c19a9c40e9af9
Jason Arritt <jsn.arritt@gmail.com>
2022-05-07 15:28:47 -0400

    Added frame for APIs

--- server.js
+++ server.js
@@ -9,11 +9,37 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
+const noteData = require('./db/db.json');
 
+// Parses incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parses incoming JSON data
+app.use(express.json());
+// Uses public/ folder for Express
 app.use(express.static("public"));
 
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+
+
+// POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+
+
+
+// ---BONUS---: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+
+
+
+// GET /notes should return the notes.html file.
+
+
+// GET * should return the index.html file.
+
+
+
+
+// Shhh, listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
