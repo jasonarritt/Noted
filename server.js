f0b38bad7bebb8ca6515c6ab271694c70642fc84
Jason Arritt <jsn.arritt@gmail.com>
2022-05-12 13:54:53 -0400

    Note POST works for gathering new note info

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-
 // Required modules
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const uuid = require('uuid');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-const notes = require('./db/db.json');
+const notes = require("./db/db.json");
 
 // Parses incoming string or array data
 app.use(express.urlencoded({ extended: true }));
@@ -18,38 +17,30 @@ app.use(express.json());
 // Uses public/ folder for Express
 app.use(express.static("public"));
 
-
 // GET /api/notes should read the db.json file and return all saved notes as JSON.
-app.get('/api/notes', (req, res) => {
-    res.json(notes);
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
 });
 
-
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-app.post('/api/notes', (req, res) => {
-
-})
-
-
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+  console.log(newNote.title, newNote.text);
+});
 
 // ---BONUS---: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 
-
-
 // GET /notes should return the notes.html file.
-app.get('/notes', (req, res) => {
-    res.sendFile(__dirname + '/public/notes.html');
-})
-
+app.get("/notes", (req, res) => {
+  res.sendFile(__dirname + "/public/notes.html");
+});
 
 // GET * should return the index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-
-
-// Shhh, listen
+// Shhh, Listen
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
