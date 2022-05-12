4f2579b0c3ecd14afbafb5c1a2a318b3d968519b
Jason Arritt <jsn.arritt@gmail.com>
2022-05-12 16:13:37 -0400

    db.json successfully updates with new notes

--- server.js
+++ server.js
@@ -25,12 +25,13 @@ app.get("/api/notes", (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post("/api/notes", (req, res) => {
   let newNote = req.body;
-
   newNote.id = uuid.v1();
+  notes.push(newNote);
 
-  console.log(newNote.title, newNote.text, newNote.id);
-
-  console.log(newNote);
+  console.log(notes);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+  });
 });
 
 // ---BONUS---: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
