199b55b9b2326d4d2a461c1f1d4cdf371ecdf933
Jason Arritt <jsn.arritt@gmail.com>
2022-05-12 14:03:26 -0400

    New notes objects are created with UUID.v1

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ app.get("/api/notes", (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
 app.post("/api/notes", (req, res) => {
   let newNote = req.body;
-  console.log(newNote.title, newNote.text);
+
+  newNote.id = uuid.v1();
+
+  console.log(newNote.title, newNote.text, newNote.id);
+
+  console.log(newNote);
 });
 
 // ---BONUS---: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
